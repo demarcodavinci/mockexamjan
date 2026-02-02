c74c3b9d6a468b2e04ad4ded73cb7768c9e893a9
demarcodavinci <denasmalysevskis@gmail.com>
2026-02-02 13:36:46 +0000

    updated files

---
+++ Task 2/mockexam/mockexam/Models/Rooms.cs
@@ -0,0 +1,14 @@
+﻿namespace mockexam.Models
+{
+    public class Rooms
+    {
+        public int RoomsId { get; set; }
+        public string RoomName { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public double HourlyRate { get; set; }
+        public string City { get; set; }
+        public bool isAvailable { get; set; }
+
+    }
+}
---
+++ Task 2/mockexam/mockexam/Models/Staff.cs
@@ -0,0 +1,11 @@
+﻿namespace mockexam.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string JobTitle { get; set; }
+        public string Bio { get; set; }
+        public string StaffFullName { get; set; }
+
+    }
+}

6fc529d6603d383ffe1f3202a58e693c0b2524b5
demarcodavinci <denasmalysevskis@gmail.com>
2026-02-06 09:34:31 +0000

    uploads

---
+++ Task 2/mockexam/mockexam/mockexam/Models/Bookings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace mockexam.Models
+{
+    public class Bookings
+    {
+        public int BookingsId { get; set; }
+        public string UserId { get; set; }
+        public Rooms? Rooms { get; set; }
+        public DateTime? CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
+        public int? NumberOfGuests { get; set; }
+        public string? Status { get; set; }
+        public DateTime BookingCreatedAt { get; set; }
+        public string? SpecialRequest {  get; set; }
+        public bool IsPayed { get; set; }
+        public DateTime? PayedAt { get; set; }
+
+        public IdentityUser? User { get; set; }
+
+    }
+}
